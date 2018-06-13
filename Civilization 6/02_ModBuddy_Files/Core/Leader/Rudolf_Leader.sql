6ad4b58fd7fd6b37b1603a20cd2ccc3877000fa2
webemich <michael.weber@wingtra.com>
2018-06-13 21:01:30 +0200

    stable Habsburg
    added Habsburg civ and leader
    updated City Names

--- Civilization 6/02_ModBuddy_Files/Core/Leader/Rudolf_Leader.sql
+++ Civilization 6/02_ModBuddy_Files/Core/Leader/Rudolf_Leader.sql
@@ -7,51 +7,51 @@
 -----------------------------------------------
 INSERT INTO	Types
 		(Type,					Kind			)
-VALUES	('LEADER_TROS_STAUFFACHER',	'KIND_LEADER'	);
+VALUES	('LEADER_TROS_RUDOLF',	'KIND_LEADER'	);
 		
 -----------------------------------------------
 -- CivilizationLeaders
 -----------------------------------------------
 INSERT INTO	CivilizationLeaders
 		(CivilizationType,				LeaderType,				CapitalName				)
-VALUES	('CIVILIZATION_TROS_EIDGENOSSENSCHAFT',	'LEADER_TROS_STAUFFACHER',	'LOC_CITY_NAME_TROS_EIDGENOSSENSCHAFT_1'	);
+VALUES	('CIVILIZATION_TROS_HABSBURG',	'LEADER_TROS_RUDOLF',	'LOC_CITY_NAME_TROS_HABSBURG_1'	);
 
 -----------------------------------------------
 -- Leaders
 -----------------------------------------------
 INSERT INTO	Leaders
 		(LeaderType,			Name,							InheritFrom,		SceneLayers		)
-VALUES	('LEADER_TROS_STAUFFACHER',	'LOC_LEADER_TROS_STAUFFACHER_NAME',	'LEADER_DEFAULT', 	4				);
+VALUES	('LEADER_TROS_RUDOLF',	'LOC_LEADER_TROS_RUDOLF_NAME',	'LEADER_DEFAULT', 	4				);
 		
 -----------------------------------------------
 -- LeaderQuotes
 -----------------------------------------------
 INSERT INTO	LeaderQuotes
 		(LeaderType,			Quote										)
-VALUES	('LEADER_TROS_STAUFFACHER',	'LOC_PEDIA_LEADERS_PAGE_TROS_STAUFFACHER_QUOTE'	);
+VALUES	('LEADER_TROS_RUDOLF',	'LOC_PEDIA_LEADERS_PAGE_TROS_RUDOLF_QUOTE'	);
 
 -----------------------------------------------
 -- LoadingInfo
 -----------------------------------------------
 INSERT INTO	LoadingInfo
 		(LeaderType,			ForegroundImage,				BackgroundImage,				PlayDawnOfManAudio	)
-VALUES	('LEADER_TROS_STAUFFACHER',	'LEADER_TROS_STAUFFACHER_NEUTRAL',	'LEADER_TROS_STAUFFACHER_BACKGROUND',	0					);
+VALUES	('LEADER_TROS_RUDOLF',	'LEADER_TROS_RUDOLF_NEUTRAL',	'LEADER_TROS_RUDOLF_BACKGROUND',	0					);
 
 -----------------------------------------------
 -- FavoredReligions
 -----------------------------------------------
 INSERT INTO FavoredReligions
 		(LeaderType,			ReligionType		)
-VALUES	('LEADER_TROS_STAUFFACHER',	'RELIGION_ISLAM'	);
+VALUES	('LEADER_TROS_RUDOLF',	'RELIGION_ISLAM'	);
 
 -----------------------------------------------
 -- Colors
 -----------------------------------------------
 INSERT INTO	PlayerColors
 		(Type,					Usage,		PrimaryColor,		 		SecondaryColor,					TextColor					)
-VALUES	('LEADER_TROS_STAUFFACHER',	'Unique',	'COLOR_TROS_STAUFFACHER_PRIMARY',	'COLOR_TROS_STAUFFACHER_SECONDARY',	'COLOR_PLAYER_WHITE_TEXT'	);
+VALUES	('LEADER_TROS_RUDOLF',	'Unique',	'COLOR_TROS_RUDOLF_PRIMARY',	'COLOR_TROS_RUDOLF_SECONDARY',	'COLOR_PLAYER_WHITE_TEXT'	);
 
 INSERT INTO	Colors
 		(Type,							Color				)
-VALUES	('COLOR_TROS_STAUFFACHER_PRIMARY',	'255,0,0,255'	),
-		('COLOR_TROS_STAUFFACHER_SECONDARY',	'255,255,255,255'		);
+VALUES	('COLOR_TROS_RUDOLF_PRIMARY',	'255,255,0,255'	),
+		('COLOR_TROS_RUDOLF_SECONDARY',	'255,0,0,255'		);
