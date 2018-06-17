2a32b3ff05822dd73fe04046155f4a8017bcaf8b
webemich <michael.weber@wingtra.com>
2018-06-17 14:52:26 +0200

    added Zuerich
    added Civ Zürich and some test

---
+++ Civilization 6/02_ModBuddy_Files/Core/Civilization/Zurich_Text.sql
@@ -0,0 +1,143 @@
+/*
+	Localisation
+*/
+
+-----------------------------------------------
+-- Types
+-----------------------------------------------
+INSERT OR REPLACE INTO LocalizedText
+		(Tag,	Language,	Text)
+VALUES
+
+-----------------------------------------------
+-- Civilization
+-----------------------------------------------
+	("LOC_CIVILIZATION_TROS_ZURICH_NAME",			"en_US",	"Zürich"			),
+	("LOC_CIVILIZATION_TROS_ZURICH_DESCRIPTION",	"en_US",	"Zürich"	),
+	("LOC_CIVILIZATION_TROS_ZURICH_ADJECTIVE",  	"en_US",	"zürcherisch"			),
+
+-----------------------------------------------
+-- UA
+-----------------------------------------------
+	("LOC_TRAIT_CIVILIZATION_TROS_ZURICH_UA_NAME",  "en_US",	"Unique Ability"	),
+	("LOC_TRAIT_CIVILIZATION_TROS_ZURICH_UA_DESCRIPTION",	"en_US",
+	"Plaecholder."	),
+
+-----------------------------------------------
+-- UI
+-----------------------------------------------
+	("LOC_DISTRICT_TROS_ZURICH_UI_NAME",	"en_US",	"Grossbank"	),
+	("LOC_DISTRICT_TROS_ZURICH_UI_DESCRIPTION",	"en_US",
+	"Eine Bank mit mehr Umsatz und mehr Gewinn als üblich."	),
+
+-----------------------------------------------
+-- UU
+-----------------------------------------------
+	("LOC_UNIT_TROS_ZURICH_UU_NAME",	"en_US",	"Tiefbauamt"	),
+	("LOC_UNIT_TROS_ZURICH_UU_DESCRIPTION",	"en_US",
+	"Eine Werkbehörde die Bauvorhaben schneller fertig stellt."),
+
+	("LOC_MODIFIER_TROS_ZURICH_UU_ADJUST_STRENGTH_DESCRIPTION",	"en_US",	"Plaecholder."	),
+
+-----------------------------------------------
+-- Cities
+-----------------------------------------------
+	("LOC_CITY_NAME_TROS_ZURICH_1", "en_US", "Zürich"),
+	("LOC_CITY_NAME_TROS_ZURICH_2", "en_US", "Rapperswil"),
+	("LOC_CITY_NAME_TROS_ZURICH_3", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_ZURICH_4", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_ZURICH_5", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_ZURICH_6", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_ZURICH_7", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_ZURICH_8", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_ZURICH_9", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_ZURICH_10", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_ZURICH_11", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_ZURICH_12", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_ZURICH_13", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_ZURICH_14", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_ZURICH_15", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_ZURICH_16", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_ZURICH_17", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_ZURICH_18", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_ZURICH_19", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_ZURICH_20", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_ZURICH_21", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_ZURICH_22", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_ZURICH_23", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_ZURICH_24", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_ZURICH_25", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_ZURICH_26", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_ZURICH_27", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_ZURICH_28", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_ZURICH_29", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_ZURICH_30", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_ZURICH_31", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_ZURICH_32", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_ZURICH_33", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_ZURICH_34", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_ZURICH_35", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_ZURICH_36", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_ZURICH_37", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_ZURICH_38", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_ZURICH_39", "en_US", "Plaecholder"),
+
+-----------------------------------------------
+-- Citizens
+-----------------------------------------------
+	("LOC_CITIZEN_TROS_ZURICH_MALE_1",		"en_US",	"Fredi"		),
+	("LOC_CITIZEN_TROS_ZURICH_MALE_2",		"en_US",	"Hans"		),
+	("LOC_CITIZEN_TROS_ZURICH_MALE_3",		"en_US",	"Placeholder"	),
+	("LOC_CITIZEN_TROS_ZURICH_MALE_4",		"en_US",	"Placeholder"	),
+	("LOC_CITIZEN_TROS_ZURICH_MALE_5",		"en_US",	"Placeholder"	),
+	("LOC_CITIZEN_TROS_ZURICH_MALE_6",		"en_US",	"Placeholder"	),
+	("LOC_CITIZEN_TROS_ZURICH_MALE_7",		"en_US",	"Placeholder"	),
+	("LOC_CITIZEN_TROS_ZURICH_MALE_8",		"en_US",	"Placeholder"	),
+	("LOC_CITIZEN_TROS_ZURICH_MALE_9",		"en_US",	"Placeholder"	),
+	("LOC_CITIZEN_TROS_ZURICH_MALE_10",		"en_US",	"Placeholder"	),
+	("LOC_CITIZEN_TROS_ZURICH_FEMALE_1",		"en_US",	"Lydia"	),
+	("LOC_CITIZEN_TROS_ZURICH_FEMALE_2",		"en_US",	"Placeholder"	),
+	("LOC_CITIZEN_TROS_ZURICH_FEMALE_3",		"en_US",	"Placeholder"	),
+	("LOC_CITIZEN_TROS_ZURICH_FEMALE_4",		"en_US",	"Placeholder"	),
+	("LOC_CITIZEN_TROS_ZURICH_FEMALE_5",		"en_US",	"Placeholder"	),
+	("LOC_CITIZEN_TROS_ZURICH_FEMALE_6",		"en_US",	"Placeholder"	),
+	("LOC_CITIZEN_TROS_ZURICH_FEMALE_7",		"en_US",	"Placeholder"	),
+	("LOC_CITIZEN_TROS_ZURICH_FEMALE_8",		"en_US",	"Placeholder"	),
+	("LOC_CITIZEN_TROS_ZURICH_FEMALE_9",		"en_US",	"Placeholder"	),
+	("LOC_CITIZEN_TROS_ZURICH_FEMALE_10",	"en_US",	"Placeholder"	),
+
+-----------------------------------------------
+-- Info
+-----------------------------------------------
+	("LOC_CIVINFO_TROS_ZURICH_LOCATION",		"en_US",	"Europe"	),
+	("LOC_CIVINFO_TROS_ZURICH_SIZE",			"en_US",	"1'728.95 km2"	),
+	("LOC_CIVINFO_TROS_ZURICH_POPULATION",		"en_US",	"1'500'000"	),
+	("LOC_CIVINFO_TROS_ZURICH_CAPITAL",			"en_US",	"Zürich"	),
+
+-----------------------------------------------
+-- Pedia
+-----------------------------------------------
+	("LOC_PEDIA_CIVILIZATIONS_PAGE_CIVILIZATION_TROS_ZURICH_CHAPTER_HISTORY_PARA_1",	"en_US",
+	"Plaecholder."),
+	("LOC_PEDIA_CIVILIZATIONS_PAGE_CIVILIZATION_TROS_ZURICH_CHAPTER_HISTORY_PARA_2",	"en_US",
+	"Plaecholder."),
+	("LOC_PEDIA_CIVILIZATIONS_PAGE_CIVILIZATION_TROS_ZURICH_CHAPTER_HISTORY_PARA_3",	"en_US",
+	"Plaecholder."),
+	("LOC_PEDIA_CIVILIZATIONS_PAGE_CIVILIZATION_TROS_ZURICH_CHAPTER_HISTORY_PARA_4",	"en_US",
+	"Plaecholder."),
+	("LOC_PEDIA_CIVILIZATIONS_PAGE_CIVILIZATION_TROS_ZURICH_CHAPTER_HISTORY_PARA_5",	"en_US",
+	"Plaecholder."),
+	("LOC_PEDIA_CIVILIZATIONS_PAGE_CIVILIZATION_TROS_ZURICH_CHAPTER_HISTORY_PARA_6",	"en_US",
+	"Plaecholder."),
+	("LOC_PEDIA_CIVILIZATIONS_PAGE_CIVILIZATION_TROS_ZURICH_CHAPTER_HISTORY_PARA_7",	"en_US",
+	"Plaecholder."),
+	("LOC_PEDIA_CIVILIZATIONS_PAGE_CIVILIZATION_TROS_ZURICH_CHAPTER_HISTORY_PARA_8",	"en_US",
+	"Plaecholder."),
+
+	("LOC_PEDIA_DISTRICTS_PAGE_DISTRICT_TROS_ZURICH_UI_CHAPTER_HISTORY_PARA_1",	"en_US",
+	"Plaecholder."),
+	("LOC_PEDIA_DISTRICTS_PAGE_DISTRICT_TROS_ZURICH_UI_CHAPTER_HISTORY_PARA_2",	"en_US",
+	"Plaecholder."),
+
+	("LOC_PEDIA_UNITS_PAGE_UNIT_TROS_ZURICH_UU_CHAPTER_HISTORY_PARA_1",  "en_US",
+	"Plaecholder.");
