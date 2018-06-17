7c01b525a08ca3ff71a8a78f42c681f08013f15b
webemich <michael.weber@wingtra.com>
2018-06-17 17:23:39 +0200

    added Burgund
    neue Civ

---
+++ Civilization 6/02_ModBuddy_Files/Core/Civilization/Burgund_Text.sql
@@ -0,0 +1,143 @@
+/*
+	Localisation
+*/
+
+-----------------------------------------------
+-- Types
+-----------------------------------------------
+INSERT OR REPLACE INTO LocalizedText
+		(Tag,	Language,	Text)
+VALUES
+
+-----------------------------------------------
+-- Civilization
+-----------------------------------------------
+	("LOC_CIVILIZATION_TROS_BURGUND_NAME",			"en_US",	"Burgund"			),
+	("LOC_CIVILIZATION_TROS_BURGUND_DESCRIPTION",	"en_US",	"Herzogtum Burgund"	),
+	("LOC_CIVILIZATION_TROS_BURGUND_ADJECTIVE",  	"en_US",	"burgundisch"			),
+
+-----------------------------------------------
+-- UA
+-----------------------------------------------
+	("LOC_TRAIT_CIVILIZATION_TROS_BURGUND_UA_NAME",  "en_US",	"Unique Ability"	),
+	("LOC_TRAIT_CIVILIZATION_TROS_BURGUND_UA_DESCRIPTION",	"en_US",
+	"Plaecholder."	),
+
+-----------------------------------------------
+-- UI
+-----------------------------------------------
+	("LOC_DISTRICT_TROS_BURGUND_UI_NAME",	"en_US",	"Burgunder Spezialgebaeude TBD"	),
+	("LOC_DISTRICT_TROS_BURGUND_UI_DESCRIPTION",	"en_US",
+	"Burgunder Spezialgebaeude TBD"	),
+
+-----------------------------------------------
+-- UU
+-----------------------------------------------
+	("LOC_UNIT_TROS_BURGUND_UU_NAME",	"en_US",	"Burgunder Spezialeinheit TBD"	),
+	("LOC_UNIT_TROS_BURGUND_UU_DESCRIPTION",	"en_US",
+	"Burgunder Spezialeinheit TBD"),
+
+	("LOC_MODIFIER_TROS_BURGUND_UU_ADJUST_STRENGTH_DESCRIPTION",	"en_US",	"Plaecholder."	),
+
+-----------------------------------------------
+-- Cities
+-----------------------------------------------
+	("LOC_CITY_NAME_TROS_BURGUND_1", "en_US", "Dijon"),
+	("LOC_CITY_NAME_TROS_BURGUND_2", "en_US", "Dole"),
+	("LOC_CITY_NAME_TROS_BURGUND_3", "en_US", "Montbéliard"),
+	("LOC_CITY_NAME_TROS_BURGUND_4", "en_US", "Oyonnax"),
+	("LOC_CITY_NAME_TROS_BURGUND_5", "en_US", "Pontarlier"),
+	("LOC_CITY_NAME_TROS_BURGUND_6", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_BURGUND_7", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_BURGUND_8", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_BURGUND_9", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_BURGUND_10", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_BURGUND_11", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_BURGUND_12", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_BURGUND_13", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_BURGUND_14", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_BURGUND_15", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_BURGUND_16", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_BURGUND_17", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_BURGUND_18", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_BURGUND_19", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_BURGUND_20", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_BURGUND_21", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_BURGUND_22", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_BURGUND_23", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_BURGUND_24", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_BURGUND_25", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_BURGUND_26", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_BURGUND_27", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_BURGUND_28", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_BURGUND_29", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_BURGUND_30", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_BURGUND_31", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_BURGUND_32", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_BURGUND_33", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_BURGUND_34", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_BURGUND_35", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_BURGUND_36", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_BURGUND_37", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_BURGUND_38", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_BURGUND_39", "en_US", "Plaecholder"),
+
+-----------------------------------------------
+-- Citizens
+-----------------------------------------------
+	("LOC_CITIZEN_TROS_BURGUND_MALE_1",		"en_US",	"Charles"		),
+	("LOC_CITIZEN_TROS_BURGUND_MALE_2",		"en_US",	"Placeholder"		),
+	("LOC_CITIZEN_TROS_BURGUND_MALE_3",		"en_US",	"Placeholder"	),
+	("LOC_CITIZEN_TROS_BURGUND_MALE_4",		"en_US",	"Placeholder"	),
+	("LOC_CITIZEN_TROS_BURGUND_MALE_5",		"en_US",	"Placeholder"	),
+	("LOC_CITIZEN_TROS_BURGUND_MALE_6",		"en_US",	"Placeholder"	),
+	("LOC_CITIZEN_TROS_BURGUND_MALE_7",		"en_US",	"Placeholder"	),
+	("LOC_CITIZEN_TROS_BURGUND_MALE_8",		"en_US",	"Placeholder"	),
+	("LOC_CITIZEN_TROS_BURGUND_MALE_9",		"en_US",	"Placeholder"	),
+	("LOC_CITIZEN_TROS_BURGUND_MALE_10",		"en_US",	"Placeholder"	),
+	("LOC_CITIZEN_TROS_BURGUND_FEMALE_1",		"en_US",	"Catherine"	),
+	("LOC_CITIZEN_TROS_BURGUND_FEMALE_2",		"en_US",	"Placeholder"	),
+	("LOC_CITIZEN_TROS_BURGUND_FEMALE_3",		"en_US",	"Placeholder"	),
+	("LOC_CITIZEN_TROS_BURGUND_FEMALE_4",		"en_US",	"Placeholder"	),
+	("LOC_CITIZEN_TROS_BURGUND_FEMALE_5",		"en_US",	"Placeholder"	),
+	("LOC_CITIZEN_TROS_BURGUND_FEMALE_6",		"en_US",	"Placeholder"	),
+	("LOC_CITIZEN_TROS_BURGUND_FEMALE_7",		"en_US",	"Placeholder"	),
+	("LOC_CITIZEN_TROS_BURGUND_FEMALE_8",		"en_US",	"Placeholder"	),
+	("LOC_CITIZEN_TROS_BURGUND_FEMALE_9",		"en_US",	"Placeholder"	),
+	("LOC_CITIZEN_TROS_BURGUND_FEMALE_10",	"en_US",	"Placeholder"	),
+
+-----------------------------------------------
+-- Info
+-----------------------------------------------
+	("LOC_CIVINFO_TROS_BURGUND_LOCATION",		"en_US",	"Europe"	),
+	("LOC_CIVINFO_TROS_BURGUND_SIZE",			"en_US",	"?"	),
+	("LOC_CIVINFO_TROS_BURGUND_POPULATION",		"en_US",	"?"	),
+	("LOC_CIVINFO_TROS_BURGUND_CAPITAL",			"en_US",	"?"	),
+
+-----------------------------------------------
+-- Pedia
+-----------------------------------------------
+	("LOC_PEDIA_CIVILIZATIONS_PAGE_CIVILIZATION_TROS_BURGUND_CHAPTER_HISTORY_PARA_1",	"en_US",
+	"Plaecholder."),
+	("LOC_PEDIA_CIVILIZATIONS_PAGE_CIVILIZATION_TROS_BURGUND_CHAPTER_HISTORY_PARA_2",	"en_US",
+	"Plaecholder."),
+	("LOC_PEDIA_CIVILIZATIONS_PAGE_CIVILIZATION_TROS_BURGUND_CHAPTER_HISTORY_PARA_3",	"en_US",
+	"Plaecholder."),
+	("LOC_PEDIA_CIVILIZATIONS_PAGE_CIVILIZATION_TROS_BURGUND_CHAPTER_HISTORY_PARA_4",	"en_US",
+	"Plaecholder."),
+	("LOC_PEDIA_CIVILIZATIONS_PAGE_CIVILIZATION_TROS_BURGUND_CHAPTER_HISTORY_PARA_5",	"en_US",
+	"Plaecholder."),
+	("LOC_PEDIA_CIVILIZATIONS_PAGE_CIVILIZATION_TROS_BURGUND_CHAPTER_HISTORY_PARA_6",	"en_US",
+	"Plaecholder."),
+	("LOC_PEDIA_CIVILIZATIONS_PAGE_CIVILIZATION_TROS_BURGUND_CHAPTER_HISTORY_PARA_7",	"en_US",
+	"Plaecholder."),
+	("LOC_PEDIA_CIVILIZATIONS_PAGE_CIVILIZATION_TROS_BURGUND_CHAPTER_HISTORY_PARA_8",	"en_US",
+	"Plaecholder."),
+
+	("LOC_PEDIA_DISTRICTS_PAGE_DISTRICT_TROS_BURGUND_UI_CHAPTER_HISTORY_PARA_1",	"en_US",
+	"Plaecholder."),
+	("LOC_PEDIA_DISTRICTS_PAGE_DISTRICT_TROS_BURGUND_UI_CHAPTER_HISTORY_PARA_2",	"en_US",
+	"Plaecholder."),
+
+	("LOC_PEDIA_UNITS_PAGE_UNIT_TROS_BURGUND_UU_CHAPTER_HISTORY_PARA_1",  "en_US",
+	"Plaecholder.");
---
+++ Civilization 6/02_ModBuddy_Files/Core/Leader/KarldK_Leader.sql
@@ -0,0 +1,57 @@
+/*
+	Leader
+*/
+
+-----------------------------------------------
+-- Types
+-----------------------------------------------
+INSERT INTO	Types
+		(Type,					Kind			)
+VALUES	('LEADER_TROS_KARLDK',	'KIND_LEADER'	);
+		
+-----------------------------------------------
+-- CivilizationLeaders
+-----------------------------------------------
+INSERT INTO	CivilizationLeaders
+		(CivilizationType,				LeaderType,				CapitalName				)
+VALUES	('CIVILIZATION_TROS_BURGUND',	'LEADER_TROS_KARLDK',	'LOC_CITY_NAME_TROS_BURGUND_1'	);
+
+-----------------------------------------------
+-- Leaders
+-----------------------------------------------
+INSERT INTO	Leaders
+		(LeaderType,			Name,							InheritFrom,		SceneLayers		)
+VALUES	('LEADER_TROS_KARLDK',	'LOC_LEADER_TROS_KARLDK_NAME',	'LEADER_DEFAULT', 	4				);
+		
+-----------------------------------------------
+-- LeaderQuotes
+-----------------------------------------------
+INSERT INTO	LeaderQuotes
+		(LeaderType,			Quote										)
+VALUES	('LEADER_TROS_KARLDK',	'LOC_PEDIA_LEADERS_PAGE_TROS_KARLDK_QUOTE'	);
+
+-----------------------------------------------
+-- LoadingInfo
+-----------------------------------------------
+INSERT INTO	LoadingInfo
+		(LeaderType,			ForegroundImage,				BackgroundImage,				PlayDawnOfManAudio	)
+VALUES	('LEADER_TROS_KARLDK',	'LEADER_TROS_KARLDK_NEUTRAL',	'LEADER_TROS_KARLDK_BACKGROUND',	0					);
+
+-----------------------------------------------
+-- FavoredReligions
+-----------------------------------------------
+INSERT INTO FavoredReligions
+		(LeaderType,			ReligionType		)
+VALUES	('LEADER_TROS_KARLDK',	'RELIGION_ISLAM'	);
+
+-----------------------------------------------
+-- Colors
+-----------------------------------------------
+INSERT INTO	PlayerColors
+		(Type,					Usage,		PrimaryColor,		 		SecondaryColor,					TextColor					)
+VALUES	('LEADER_TROS_KARLDK',	'Unique',	'COLOR_TROS_KARLDK_PRIMARY',	'COLOR_TROS_KARLDK_SECONDARY',	'COLOR_PLAYER_WHITE_TEXT'	);
+
+INSERT INTO	Colors
+		(Type,							Color				)
+VALUES	('COLOR_TROS_KARLDK_PRIMARY',	'0,149,255,255'	),
+		('COLOR_TROS_KARLDK_SECONDARY',	'255,255,0,255'		);
