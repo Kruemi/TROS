7b706d5a6ad0bfeecbb617cebb285bb350a2c1c1
webemich <michael.weber@wingtra.com>
2018-07-07 00:27:26 +0200

    Savoyen und Bern
    zwei neue Civs

---
+++ Civilization 6/02_ModBuddy_Files/Core/Civilization/Savoyen_Text.sql
@@ -0,0 +1,143 @@
+/*
+	Localisation
+*/
+
+-----------------------------------------------
+-- Types
+-----------------------------------------------
+INSERT OR REPLACE INTO LocalizedText
+		(Tag,	Language,	Text)
+VALUES
+
+-----------------------------------------------
+-- Civilization
+-----------------------------------------------
+	("LOC_CIVILIZATION_TROS_SAVOYEN_NAME",			"en_US",	"Savoyen"			),
+	("LOC_CIVILIZATION_TROS_SAVOYEN_DESCRIPTION",	"en_US",	"Herzogtum Savoyen"	),
+	("LOC_CIVILIZATION_TROS_SAVOYEN_ADJECTIVE",  	"en_US",	"savoy"			),
+
+-----------------------------------------------
+-- UA
+-----------------------------------------------
+	("LOC_TRAIT_CIVILIZATION_TROS_SAVOYEN_UA_NAME",  "en_US",	"Unique Ability"	),
+	("LOC_TRAIT_CIVILIZATION_TROS_SAVOYEN_UA_DESCRIPTION",	"en_US",
+	"Plaecholder."	),
+
+-----------------------------------------------
+-- UI
+-----------------------------------------------
+	("LOC_DISTRICT_TROS_SAVOYEN_UI_NAME",	"en_US",	"Einzigartiges Gebäude von Savoyen"	),
+	("LOC_DISTRICT_TROS_SAVOYEN_UI_DESCRIPTION",	"en_US",
+	"Beschreibung"	),
+
+-----------------------------------------------
+-- UU
+-----------------------------------------------
+	("LOC_UNIT_TROS_SAVOYEN_UU_NAME",	"en_US",	"Einzigartige Einheit von Savoyen"	),
+	("LOC_UNIT_TROS_SAVOYEN_UU_DESCRIPTION",	"en_US",
+	"Beschreibung"),
+
+	("LOC_MODIFIER_TROS_SAVOYEN_UU_ADJUST_STRENGTH_DESCRIPTION",	"en_US",	"Plaecholder."	),
+
+-----------------------------------------------
+-- Cities
+-----------------------------------------------
+	("LOC_CITY_NAME_TROS_SAVOYEN_1", "en_US", "Chambery"),
+	("LOC_CITY_NAME_TROS_SAVOYEN_2", "en_US", "Annecy"),
+	("LOC_CITY_NAME_TROS_SAVOYEN_3", "en_US", "Vevey"),
+	("LOC_CITY_NAME_TROS_SAVOYEN_4", "en_US", "Yverdon"),
+	("LOC_CITY_NAME_TROS_SAVOYEN_5", "en_US", "Evian"),
+	("LOC_CITY_NAME_TROS_SAVOYEN_6", "en_US", "Bourg-en-Bresse"),
+	("LOC_CITY_NAME_TROS_SAVOYEN_7", "en_US", "Monthey"),
+	("LOC_CITY_NAME_TROS_SAVOYEN_8", "en_US", "Martigny"),
+	("LOC_CITY_NAME_TROS_SAVOYEN_9", "en_US", "Aosta"),
+	("LOC_CITY_NAME_TROS_SAVOYEN_10", "en_US", "Nyon"),
+	("LOC_CITY_NAME_TROS_SAVOYEN_11", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_SAVOYEN_12", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_SAVOYEN_13", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_SAVOYEN_14", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_SAVOYEN_15", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_SAVOYEN_16", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_SAVOYEN_17", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_SAVOYEN_18", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_SAVOYEN_19", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_SAVOYEN_20", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_SAVOYEN_21", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_SAVOYEN_22", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_SAVOYEN_23", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_SAVOYEN_24", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_SAVOYEN_25", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_SAVOYEN_26", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_SAVOYEN_27", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_SAVOYEN_28", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_SAVOYEN_29", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_SAVOYEN_30", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_SAVOYEN_31", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_SAVOYEN_32", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_SAVOYEN_33", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_SAVOYEN_34", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_SAVOYEN_35", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_SAVOYEN_36", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_SAVOYEN_37", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_SAVOYEN_38", "en_US", "Plaecholder"),
+	("LOC_CITY_NAME_TROS_SAVOYEN_39", "en_US", "Plaecholder"),
+
+-----------------------------------------------
+-- Citizens
+-----------------------------------------------
+	("LOC_CITIZEN_TROS_SAVOYEN_MALE_1",		"en_US",	"Viktor"		),
+	("LOC_CITIZEN_TROS_SAVOYEN_MALE_2",		"en_US",	"Placeholder"		),
+	("LOC_CITIZEN_TROS_SAVOYEN_MALE_3",		"en_US",	"Placeholder"	),
+	("LOC_CITIZEN_TROS_SAVOYEN_MALE_4",		"en_US",	"Placeholder"	),
+	("LOC_CITIZEN_TROS_SAVOYEN_MALE_5",		"en_US",	"Placeholder"	),
+	("LOC_CITIZEN_TROS_SAVOYEN_MALE_6",		"en_US",	"Placeholder"	),
+	("LOC_CITIZEN_TROS_SAVOYEN_MALE_7",		"en_US",	"Placeholder"	),
+	("LOC_CITIZEN_TROS_SAVOYEN_MALE_8",		"en_US",	"Placeholder"	),
+	("LOC_CITIZEN_TROS_SAVOYEN_MALE_9",		"en_US",	"Placeholder"	),
+	("LOC_CITIZEN_TROS_SAVOYEN_MALE_10",		"en_US",	"Placeholder"	),
+	("LOC_CITIZEN_TROS_SAVOYEN_FEMALE_1",		"en_US",	"Placeholder"	),
+	("LOC_CITIZEN_TROS_SAVOYEN_FEMALE_2",		"en_US",	"Placeholder"	),
+	("LOC_CITIZEN_TROS_SAVOYEN_FEMALE_3",		"en_US",	"Placeholder"	),
+	("LOC_CITIZEN_TROS_SAVOYEN_FEMALE_4",		"en_US",	"Placeholder"	),
+	("LOC_CITIZEN_TROS_SAVOYEN_FEMALE_5",		"en_US",	"Placeholder"	),
+	("LOC_CITIZEN_TROS_SAVOYEN_FEMALE_6",		"en_US",	"Placeholder"	),
+	("LOC_CITIZEN_TROS_SAVOYEN_FEMALE_7",		"en_US",	"Placeholder"	),
+	("LOC_CITIZEN_TROS_SAVOYEN_FEMALE_8",		"en_US",	"Placeholder"	),
+	("LOC_CITIZEN_TROS_SAVOYEN_FEMALE_9",		"en_US",	"Placeholder"	),
+	("LOC_CITIZEN_TROS_SAVOYEN_FEMALE_10",	"en_US",	"Placeholder"	),
+
+-----------------------------------------------
+-- Info
+-----------------------------------------------
+	("LOC_CIVINFO_TROS_SAVOYEN_LOCATION",		"en_US",	"Europe"	),
+	("LOC_CIVINFO_TROS_SAVOYEN_SIZE",			"en_US",	"?"	),
+	("LOC_CIVINFO_TROS_SAVOYEN_POPULATION",		"en_US",	"?"	),
+	("LOC_CIVINFO_TROS_SAVOYEN_CAPITAL",			"en_US",	"?"	),
+
+-----------------------------------------------
+-- Pedia
+-----------------------------------------------
+	("LOC_PEDIA_CIVILIZATIONS_PAGE_CIVILIZATION_TROS_SAVOYEN_CHAPTER_HISTORY_PARA_1",	"en_US",
+	"Plaecholder."),
+	("LOC_PEDIA_CIVILIZATIONS_PAGE_CIVILIZATION_TROS_SAVOYEN_CHAPTER_HISTORY_PARA_2",	"en_US",
+	"Plaecholder."),
+	("LOC_PEDIA_CIVILIZATIONS_PAGE_CIVILIZATION_TROS_SAVOYEN_CHAPTER_HISTORY_PARA_3",	"en_US",
+	"Plaecholder."),
+	("LOC_PEDIA_CIVILIZATIONS_PAGE_CIVILIZATION_TROS_SAVOYEN_CHAPTER_HISTORY_PARA_4",	"en_US",
+	"Plaecholder."),
+	("LOC_PEDIA_CIVILIZATIONS_PAGE_CIVILIZATION_TROS_SAVOYEN_CHAPTER_HISTORY_PARA_5",	"en_US",
+	"Plaecholder."),
+	("LOC_PEDIA_CIVILIZATIONS_PAGE_CIVILIZATION_TROS_SAVOYEN_CHAPTER_HISTORY_PARA_6",	"en_US",
+	"Plaecholder."),
+	("LOC_PEDIA_CIVILIZATIONS_PAGE_CIVILIZATION_TROS_SAVOYEN_CHAPTER_HISTORY_PARA_7",	"en_US",
+	"Plaecholder."),
+	("LOC_PEDIA_CIVILIZATIONS_PAGE_CIVILIZATION_TROS_SAVOYEN_CHAPTER_HISTORY_PARA_8",	"en_US",
+	"Plaecholder."),
+
+	("LOC_PEDIA_DISTRICTS_PAGE_DISTRICT_TROS_SAVOYEN_UI_CHAPTER_HISTORY_PARA_1",	"en_US",
+	"Plaecholder."),
+	("LOC_PEDIA_DISTRICTS_PAGE_DISTRICT_TROS_SAVOYEN_UI_CHAPTER_HISTORY_PARA_2",	"en_US",
+	"Plaecholder."),
+
+	("LOC_PEDIA_UNITS_PAGE_UNIT_TROS_SAVOYEN_UU_CHAPTER_HISTORY_PARA_1",  "en_US",
+	"Plaecholder.");
---
+++ Civilization 6/02_ModBuddy_Files/Core/Leader/Viktor_Leader.sql
@@ -0,0 +1,57 @@
+/*
+	Leader
+*/
+
+-----------------------------------------------
+-- Types
+-----------------------------------------------
+INSERT INTO	Types
+		(Type,					Kind			)
+VALUES	('LEADER_TROS_VIKTOR',	'KIND_LEADER'	);
+		
+-----------------------------------------------
+-- CivilizationLeaders
+-----------------------------------------------
+INSERT INTO	CivilizationLeaders
+		(CivilizationType,				LeaderType,				CapitalName				)
+VALUES	('CIVILIZATION_TROS_SAVOYEN',	'LEADER_TROS_VIKTOR',	'LOC_CITY_NAME_TROS_SAVOYEN_1'	);
+
+-----------------------------------------------
+-- Leaders
+-----------------------------------------------
+INSERT INTO	Leaders
+		(LeaderType,			Name,							InheritFrom,		SceneLayers		)
+VALUES	('LEADER_TROS_VIKTOR',	'LOC_LEADER_TROS_VIKTOR_NAME',	'LEADER_DEFAULT', 	4				);
+		
+-----------------------------------------------
+-- LeaderQuotes
+-----------------------------------------------
+INSERT INTO	LeaderQuotes
+		(LeaderType,			Quote										)
+VALUES	('LEADER_TROS_VIKTOR',	'LOC_PEDIA_LEADERS_PAGE_TROS_VIKTOR_QUOTE'	);
+
+-----------------------------------------------
+-- LoadingInfo
+-----------------------------------------------
+INSERT INTO	LoadingInfo
+		(LeaderType,			ForegroundImage,				BackgroundImage,				PlayDawnOfManAudio	)
+VALUES	('LEADER_TROS_VIKTOR',	'LEADER_TROS_VIKTOR_NEUTRAL',	'LEADER_TROS_VIKTOR_BACKGROUND',	0					);
+
+-----------------------------------------------
+-- FavoredReligions
+-----------------------------------------------
+INSERT INTO FavoredReligions
+		(LeaderType,			ReligionType		)
+VALUES	('LEADER_TROS_VIKTOR',	'RELIGION_ISLAM'	);
+
+-----------------------------------------------
+-- Colors
+-----------------------------------------------
+INSERT INTO	PlayerColors
+		(Type,					Usage,		PrimaryColor,		 		SecondaryColor,					TextColor					)
+VALUES	('LEADER_TROS_VIKTOR',	'Unique',	'COLOR_TROS_VIKTOR_PRIMARY',	'COLOR_TROS_VIKTOR_SECONDARY',	'COLOR_PLAYER_WHITE_TEXT'	);
+
+INSERT INTO	Colors
+		(Type,							Color				)
+VALUES	('COLOR_TROS_VIKTOR_PRIMARY',	'200,50,50,255'	),
+		('COLOR_TROS_VIKTOR_SECONDARY',	'230,230,230,255'		);
