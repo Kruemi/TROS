d0562a5eb095c9f3ab70f044479d02015f03827b
Kruemi <4376585+Kruemi@users.noreply.github.com>
2019-01-08 18:28:08 +0100

    in Arbeit: Funktion zum Hinzufügen von Gebäunden

--- Civilization 6/02_ModBuddy_Files/Scenario/TROS_Scenario.lua
+++ Civilization 6/02_ModBuddy_Files/Scenario/TROS_Scenario.lua
@@ -12,7 +12,11 @@
 
 -- global variables
 g_iW, g_iH = Map.GetGridSize();
+pCitiesList = {};
 
+--[[ Firaxis Games developers quotes
+I do not know why we make local functions, but I am keeping standard
+]]
 
 -- ===========================================================================
 -- Initialization functions
@@ -22,6 +26,7 @@ function Initialize()
 	print ("Initialize");
 	
 	IdentifyCivilizationIDs()
+	CreateCitiesTable()
 end
 
 -- assigns ID to corresponding civilization, identifies user
@@ -62,6 +67,38 @@ function IdentifyCivilizationIDs()
 	end
 end
 
+-- create table of cities with city name as key (format: "Altdorf" and "LOC_CITY_NAME_TROS_EIDGENOSSENSCHAFT_1")
+function CreateCitiesTable()
+	print ("CreateCitiesTable");
+
+	-- pCitiesList = {}; -- initialize empty table
+	
+	for iPlayer = 0,62 do 
+		local pCurrPlayer = Players[iPlayer]
+		if pCurrPlayer:WasEverAlive() then
+			local sPlayerCivName = PlayerConfigurations[iPlayer]:GetCivilizationTypeName()
+			local pCities = pCurrPlayer:GetCities()
+			local nCities = pCities:GetCount()
+			local currCapital = -1
+
+			if (iPlayer < 62) then
+				currCapitalID = pCurrPlayer:GetCities():GetCapitalCity():GetID()
+			end
+
+			for i,pCity in pCities:Members() do
+				pCitiesList[Locale.Lookup(pCity:GetName())] = pCity -- Format: Altdorf
+				pCitiesList[pCity:GetName()] = pCity -- Format: LOC_CITY_NAME_TROS_EIDGENOSSENSCHAFT_1
+
+				if (currCapitalID == pCity:GetID()) then sIsCapital = " *" else sIsCapital = "" end -- capital cities are marked with an asterisk
+
+				print("ID: " .. iPlayer .. " - Civilization: " .. sPlayerCivName .. " - City " .. i .. " of " .. nCities .. ": " .. Locale.Lookup(pCity:GetName()) .. sIsCapital .. " - " .. pCity:GetName() .. " - " .. pCity:GetID());
+			end
+		end
+	end
+
+	print ("CreateCitiesTable: finished");
+end
+
 function Initialize_NewGame()
 	print ("Initialize_NewGame");
   
@@ -77,16 +114,18 @@ function Initialize_NewGame()
 	-- Cities
 	print ("Initialize: Buildings");
 
+	AddBuilding("LOC_CITY_NAME_TROS_EIDGENOSSENSCHAFT_1", "BUILDING_SEWER", "DISTRICT_CITY_CENTER")
+
 	-- Eidgenossenschaft
 	for i, pCity in Players[eEidgenossenschaftPlayer]:GetCities():Members() do
 		local sCityName = pCity:GetName(); -- Format: LOC_CITY_NAME_TROS_EIDGENOSSENSCHAFT_1
-		
+
 		-- Add building
-		if (sCityName == "LOC_CITY_NAME_TROS_EIDGENOSSENSCHAFT_1") then
+		--[[ if (sCityName == "LOC_CITY_NAME_TROS_EIDGENOSSENSCHAFT_1") then
 			local pCityPlot = Map.GetPlot(pCity:GetX(), pCity:GetY()) -- needs to be placed in correct district
 			local pCityBuildQ = pCity:GetBuildQueue()
 			pCityBuildQ:CreateIncompleteBuilding(GameInfo.Buildings["BUILDING_SEWER"].Index, pCityPlot:GetIndex(), 100);
-		end
+		end ]]
 		
 		-- Remove building
 		if (sCityName == "LOC_CITY_NAME_TROS_EIDGENOSSENSCHAFT_2") then
@@ -102,6 +141,52 @@ function Initialize_NewGame()
 	end
 
 	print("Initialize_NewGame: finished")
+end
+
+-- ===========================================================================
+-- AddBuilding
+-- Input:
+--	sCityName: "Altdorf" or "LOC_CITY_NAME_TROS_EIDGENOSSENSCHAFT_1"
+--	sBuildingName: "BUILDING_MONUMENT"
+--	sDistrictName: "DISTRICT_CITY_CENTER"
+function AddBuilding(sCityName, sBuildingName, sDistrictName)
+	print ("AddBuilding")
+	local pCity = pCitiesList[sCityName]
+	
+	if (not pCity:GetBuildings():HasBuilding(GameInfo.Buildings[sBuildingName].Index)) then
+		local pDistricts = pCity:GetDistricts()
+		local hasDistrict = false
+		print("11")
+
+		print(pCity:GetOwner())
+		for i,district in pDistricts:Members() do
+			print("22")
+			if district:IsComplete() then
+				print("33")
+				local districtInfo = GameInfo.Districts[district:GetType()]
+				if districtInfo.DistrictType == sDistrictName then
+					print("44")
+					hasDistrict = true
+					-- toolTipString = Locale.Lookup(districtInfo.Name)
+				end
+			end
+		end
+		print("55")
+
+		if (hasDistrict) then
+			
+			local pPlot = Map.GetPlot(pDistricts():GetDistrict(sDistrictName):GetX(), pDistricts():GetDistrict(sDistrictName):GetY())
+			local pCityBuildQ = pCity:GetBuildQueue()
+
+			pCityBuildQ:CreateIncompleteBuilding(GameInfo.Buildings[sBuildingName].Index, pPlot:GetIndex(), 100);
+
+			print (sBuildingName .. " added to " .. sCityName)
+		else
+			print ("Error: " .. sCityName .. " does not have the necessary district " .. sDistrictName .. " to build " .. sBuildingName)
+		end
+	else
+		print ("Warning: " .. sBuildingName .. " has already been built in " .. sCityName)
+	end
 end
 
 Initialize();
@@ -136,12 +221,27 @@ function GodMode_SetVisibility()
 	print("God Mode: finished")
 end
 
--- toggle God Mode
+-- toggle God Mode (set to false before release)
 if (true) then
 	print("Can you hear me?")
 
 	LuaEvents.NewGameInitialized.Add(Initialize_GodMode);
 end
 
+-- print table https://stackoverflow.com/questions/9168058/how-to-dump-a-table-to-console
+function dump(o)
+   print("dumping table")
+   if type(o) == 'table' then
+      local s = '{ '
+      for k,v in pairs(o) do
+         if type(k) ~= 'number' then k = '"'..k..'"' end
+         s = s .. '['..k..'] = ' .. dump(v) .. ','
+      end
+      return s .. '} '
+   else
+      return tostring(o)
+   end
+   print("finished dumping table")
+end
 
 
