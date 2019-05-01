363047ce7f46efbdbddeede64d0a52f75d5d5434
Kruemi <4376585+Kruemi@users.noreply.github.com>
2019-05-01 14:06:30 +0200

    Lua-Files mit Spring-Update kompatibel gemacht
    NEU: 'ReplaceUIScript' muss nicht mehr von Hand in .modinfo eingefügt werden (kann Dank Update direkt in ModBuddy gelöst werden)
    
    TROS_Scenario_UI:
    
    Scenario_DeclareWar: in bestimmter Runde Krieg erklären
    BoostUnlockedPopup_TROS_Scenario.lua:
    
    überspringt Boost-Popups (eigentlich sollten die vom Spiel aus in der ersten Runde übersprungen werden, evtl. bessere Lösung möglich)
    InGame_TROS_Scenario.lua:
    
    Hotkey "N" hinzugefügt: im Spiel "N" drücken, um sofort nächste Runde zu starten (Wissenschaft, Civics, Produktion, etc. müssen nicht ausgewählt werden)

---
+++ Civilization 6/02_ModBuddy_Files/Scenario/LuaFunctions/TROS_DeclareWarPopup.lua
@@ -0,0 +1,62 @@
+﻿include( "InstanceManager" );
+include( "DiplomacyStatementSupport" );
+include( "SupportFunctions" );
+include( "Colors" );
+
+-- ===========================================================================
+function CanDeclareWar(eDefendingPlayer:number)
+	local pPlayerConfig:table = PlayerConfigurations[eDefendingPlayer];
+	return pPlayerConfig:GetCivilizationLevelTypeID() == CivilizationLevelTypes.CIVILIZATION_LEVEL_FULL_CIV;
+end
+-- ===========================================================================
+function DeclareWar(eAttackingPlayer:number, eDefendingPlayer:number, eWarType:number)
+	
+	--if CanDeclareWar(eDefendingPlayer) then
+	if true then
+
+		if (eWarType == WarTypes.SURPRISE_WAR) then 
+			DiplomacyManager.RequestSession(eAttackingPlayer, eDefendingPlayer, "DECLARE_SURPRISE_WAR");
+			
+		elseif (eWarType == WarTypes.FORMAL_WAR) then
+			DiplomacyManager.RequestSession(eAttackingPlayer, eDefendingPlayer, "DECLARE_FORMAL_WAR");
+				
+		elseif (eWarType == WarTypes.HOLY_WAR) then
+			DiplomacyManager.RequestSession(eAttackingPlayer, eDefendingPlayer, "DECLARE_HOLY_WAR");
+				
+		elseif (eWarType == WarTypes.LIBERATION_WAR) then
+			DiplomacyManager.RequestSession(eAttackingPlayer, eDefendingPlayer, "DECLARE_LIBERATION_WAR");
+			
+		elseif (eWarType == WarTypes.RECONQUEST_WAR) then
+			DiplomacyManager.RequestSession(eAttackingPlayer, eDefendingPlayer, "DECLARE_RECONQUEST_WAR");
+			
+		elseif (eWarType == WarTypes.PROTECTORATE_WAR) then
+			DiplomacyManager.RequestSession(eAttackingPlayer, eDefendingPlayer, "DECLARE_PROTECTORATE_WAR");
+			
+		elseif (eWarType == WarTypes.COLONIAL_WAR) then
+			DiplomacyManager.RequestSession(eAttackingPlayer, eDefendingPlayer, "DECLARE_COLONIAL_WAR");
+			
+		elseif (eWarType == WarTypes.TERRITORIAL_WAR) then
+			DiplomacyManager.RequestSession(eAttackingPlayer, eDefendingPlayer, "DECLARE_TERRITORIAL_WAR");
+
+		elseif (eWarType == WarTypes.SURPRISE_WAR) then
+			DiplomacyManager.RequestSession(eAttackingPlayer, eDefendingPlayer, "DECLARE_SURPRISE_WAR");
+		
+		elseif (eWarType == WarTypes.GOLDEN_AGE_WAR) then
+			DiplomacyManager.RequestSession(eAttackingPlayer, eDefendingPlayer, "DECLARE_GOLDEN_AGE_WAR");
+		
+		elseif (eWarType == WarTypes.WAR_OF_RETRIBUTION) then
+			DiplomacyManager.RequestSession(eAttackingPlayer, eDefendingPlayer, "DECLARE_WAR_OF_RETRIBUTION");
+		
+		elseif (eWarType == WarTypes.IDEOLOGICAL_WAR) then
+			DiplomacyManager.RequestSession(eAttackingPlayer, eDefendingPlayer, "DECLARE_IDEOLOGICAL_WAR");
+		
+		end
+
+	else
+		local parameters :table = {};
+		parameters[ PlayerOperations.PARAM_PLAYER_ONE ] = eAttackingPlayer;
+		parameters[ PlayerOperations.PARAM_PLAYER_TWO ] = eDefendingPlayer;
+		UI.RequestPlayerOperation(eAttackingPlayer, PlayerOperations.DIPLOMACY_DECLARE_WAR, parameters);
+		UI.PlaySound("Notification_War_Declared");
+	end
+end
---
+++ Civilization 6/02_ModBuddy_Files/Scenario/TROS_Scenario_UI.lua
@@ -0,0 +1,70 @@
+﻿--include( "InstanceManager" );
+include( "SupportFunctions" );
+include( "DiplomacyStatementSupport" );
+--include( "DeclareWarPopup" );
+--include( "DeclareWarPopup_Expansion1" );
+include( "TROS_DeclareWarPopup" );
+
+-- ===========================================================================
+-- OnPlayerTurnActivated
+-- for examples see:
+--	AlexanderScenario, AustraliaScenario, IndonesiaKhmerScenario, NubiaScenario, PolandScenario, PolandScenario\UI\UnitFlagManager, VikingScenario, Expansion1\UI\CityBanners
+function OnPlayerTurnActivated(currPlayerID, isFirstTimeThisTurn)
+	local currTurn = Game.GetCurrentGameTurn()
+	local aPlayers = PlayerManager.GetAlive()
+
+	-- ===========================================================================
+	-- Diplomacy
+	
+	--[[ -------------------------------------------------------------------------
+
+	List of functions:
+
+	Scenario_DeclareWar(iDeclarationTurn, eAttackingPlayer, eDefendingPlayer, eWarType, currPlayerID, currTurn)
+		Input:
+			iDeclarationTurn: 14
+			eAttackingPlayer: ExposedMembers.eSavoyenPlayer
+			eDefendingPlayer: ExposedMembers.eEidgenossenschaftPlayer
+			eWarType: WarTypes.FORMAL_WAR
+			currPlayerID: currPlayerID
+			currTurn: currTurn
+
+	]] ---------------------------------------------------------------------------
+
+	-- Wars ----------------------------------------------------------------------
+	--	WarTypes: SURPRISE_WAR, FORMAL_WAR, HOLY_WAR, RECONQUEST_WAR, LIBERATION_WAR, DEFENSIVE_PACT, JOINT_WAR, SUZERAIN_WAR, PROTECTORATE_WAR, COLONIAL_WAR, TERRITORIAL_WAR, NO_WAR, DECLARE_GOLDEN_AGE_WAR, DECLARE_WAR_OF_RETRIBUTION, DECLARE_IDEOLOGICAL_WAR
+	--  NOTE: same lines ("Scenario_DeclareWar(...)") need to be placed into 'TROS_Scenario.lua' under 'CheckForDiplomacyEvent'
+	Scenario_DeclareWar(1, ExposedMembers.eSavoyenPlayer, ExposedMembers.eEidgenossenschaftPlayer, WarTypes.FORMAL_WAR, currPlayerID, currTurn)
+	Scenario_DeclareWar(3, ExposedMembers.eFrancePlayer, ExposedMembers.eEidgenossenschaftPlayer, WarTypes.HOLY_WAR, currPlayerID, currTurn)
+
+end
+
+-- ===========================================================================
+-- Scenario_DeclareWar
+-- Input:
+--	iDeclarationTurn: turn in which war is declared
+--	eAttackingPlayer: integer
+--	eDefendingPlayer: integer
+--	eWarType: e.g. WarTypes.HOLY_WAR, WarTypes.FORMAL_WAR
+--	currPlayerID: integer
+--	currTurn: integer
+function Scenario_DeclareWar(iDeclarationTurn, eAttackingPlayer, eDefendingPlayer, eWarType, currPlayerID, currTurn)
+	if iDeclarationTurn == currTurn and currPlayerID == eAttackingPlayer and ExposedMembers.bWarDeclaration then
+		ExposedMembers.bWarDeclaration = false
+		DeclareWar(eAttackingPlayer, eDefendingPlayer, eWarType)
+
+		print("[Turn " .. iDeclarationTurn .. "] Scenario_DeclareWar (UI): " .. PlayerConfigurations[eAttackingPlayer]:GetCivilizationTypeName() .. " has declared a " .. eWarType .. " on " .. PlayerConfigurations[eDefendingPlayer]:GetCivilizationTypeName())
+	end
+end
+
+function Initialize()
+	print("Initialize")
+	--GameEvents.PlayerTurnStarted.Add(OnPlayerTurnActivated);
+	--Events.PlayerTurnStarted.Add(OnPlayerTurnActivated);
+	Events.PlayerTurnActivated.Add(OnPlayerTurnActivated);
+
+	print("Initialize: finished")
+end
+
+--GameEvents.PlayerTurnStarted.Add(OnPlayerTurnActivated);
+Initialize();
