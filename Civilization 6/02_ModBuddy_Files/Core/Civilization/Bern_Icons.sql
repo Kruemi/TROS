edae8f298557d163a7a61cd68b74679fffea410a
Kruemi <4376585+Kruemi@users.noreply.github.com>
2019-06-03 00:47:40 +0200

    Icon-Struktur neu aufgebaut (alle Icons einer Grösse in selber Bilddatei)
    Civilizations: alle Grössen für In-Game (Front-End: farbige 45px fehlen noch; D/F/I fehlen noch)
    Leaders: alle Grössen (Probleme mit Darstellung in Diplomatiebildschirm; D/F/I fehlen noch)

---
+++ Civilization 6/02_ModBuddy_Files/Core/Civilization/Bern_Icons.sql
@@ -0,0 +1,30 @@
+﻿/*
+	Icons
+*/
+
+-----------------------------------------------
+-- IconTextureAtlases (Civilization Icons)
+-----------------------------------------------
+INSERT INTO	IconTextureAtlases
+		(Name,									IconSize,	IconsPerRow,	IconsPerColumn,	Filename									)
+VALUES	('ICON_ATLAS_TROS_BERN',	256,		1,				1,				'ICON_CIVILIZATION_TROS_BERN_256'		);
+--		('ICON_ATLAS_CIVILIZATION_TROS_BERN',	45,			1,				1,				'ICON_CIVILIZATION_TROS_BERN_COLOR_45.dds'	);
+
+-----------------------------------------------
+-- IconDefinitions
+-----------------------------------------------
+INSERT OR REPLACE INTO	IconDefinitions
+		(Name,							Atlas,									'Index'	)
+VALUES	('ICON_CIVILIZATION_TROS_BERN',	'ICON_ATLAS_TROS_BERN',	0		);
+		--('ICON_LEADER_TROS_JAKOB',		'ICON_ATLAS_LEADER_TROS_JAKOB',			0		), -- Leader Icon
+		--('ICON_BUILDING_TROS_BERN_UI',	'ICON_ATLAS_BUILDING_TROS_BERN_UI',		0		); -- Unique Infrastructure Icon
+		-- NOTE: --might need _FOW (fog of war) for some (e.g. building)
+
+
+-----------------------------------------------
+-- IconTextureAtlases (Unique Infrastructure Icons)
+-----------------------------------------------
+--INSERT INTO	IconTextureAtlases
+--		(Name,										IconSize,	IconsPerRow,	IconsPerColumn,	Filename								)
+--VALUES	('ICON_ATLAS_BUILDING_TROS_BERN_UI',		256,		1,				1,				'ICON_BUILDING_TROS_BERN_UI_256.dds'	),
+--		('ICON_ATLAS_BUILDING_TROS_BERN_UI_FOW',	256,		1,				1,				'ICON_BUILDING_TROS_BERN_UI_256.dds'	);
