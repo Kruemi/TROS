22f901d17e46321e45889689c23217947d3c7bf4
webemich <michael.weber@wingtra.com>
2020-05-03 23:10:55 +0200

    Vorbereitung Icons
    erste Dateien erstellt

--- Civilization 6/02_ModBuddy_Files/Core/TROS_Civilizations_Icons.sql
+++ Civilization 6/02_ModBuddy_Files/Core/TROS_Civilizations_Icons.sql
@@ -18,8 +18,6 @@ VALUES	('ICON_ATLAS_TROS_CIVILIZATIONS',	256,		4,				4,				'TROS_CIVILIZATIONS_I
 		('ICON_ATLAS_TROS_CIVILIZATIONS',	22,			4,				4,				'TROS_CIVILIZATIONS_ICONS_22.dds'	);
 		--('ICON_ATLAS_TROS_CIVILIZATIONS',	45,			4,				4,				'TROS_CIVILIZATIONS_ICONS_45.dds'	);
 
-
-
 -----------------------------------------------
 -- IconDefinitions
 -----------------------------------------------
@@ -36,3 +34,30 @@ VALUES	('ICON_CIVILIZATION_TROS_EIDGENOSSENSCHAFT',	'ICON_ATLAS_TROS_CIVILIZATIO
 		('ICON_CIVILIZATION_TROS_BERN',					'ICON_ATLAS_TROS_CIVILIZATIONS',	9		),
 		('ICON_CIVILIZATION_TROS_GRAUBUNDEN',			'ICON_ATLAS_TROS_CIVILIZATIONS',	10		),
 		('ICON_CIVILIZATION_TROS_WALLIS',				'ICON_ATLAS_TROS_CIVILIZATIONS',	11		);
+
+
+
+
+
+/*
+	Icons Buidlings
+*/
+
+-----------------------------------------------
+-- IconDefinitions
+-----------------------------------------------
+INSERT INTO	IconDefinitions
+		(Name,								Atlas,							'Index'	)
+VALUES	('ICON_BUILDING_TROS_ZURICH_UI',	'ICON_ATLAS_TROS_BUILDINGS',	8		);
+
+-----------------------------------------------
+-- IconTextureAtlases
+-----------------------------------------------
+INSERT INTO	IconTextureAtlases
+		(Name,							IconSize,	IconsPerRow,	IconsPerColumn,	Filename						)
+VALUES	('ICON_ATLAS_TROS_BUILDINGS',	256,		4,				4,				'TROS_BUILDINGS_ICONS_256.dds'	),
+		('ICON_ATLAS_TROS_BUILDINGS',	128,		4,				4,				'TROS_BUILDINGS_ICONS_128.dds'	),
+		('ICON_ATLAS_TROS_BUILDINGS',	80,			4,				4,				'TROS_BUILDINGS_ICONS_80.dds'	),
+		('ICON_ATLAS_TROS_BUILDINGS',	50,			4,				4,				'TROS_BUILDINGS_ICONS_50.dds'	),
+		('ICON_ATLAS_TROS_BUILDINGS',	38,			4,				4,				'TROS_BUILDINGS_ICONS_38.dds'	),
+		('ICON_ATLAS_TROS_BUILDINGS',	32,			4,				4,				'TROS_BUILDINGS_ICONS_32.dds'	); -- 256 FOW?
